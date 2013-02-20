d3886b24f67b54106e49dcac203b413eea565724
root <root@midgard.user.openhosting.com>
2013-02-20 17:29:11 -0500

    initial commit

---
+++ kuehnel/portlets/moonphase/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+MoonphasePortletMessageFactory = MessageFactory('kuehnel.portlets.moonphase')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='kuehnel.portlets.moonphase',
+      version=version,
+      description="Plone moonphase portlet",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['kuehnel', 'kuehnel.portlets'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
